9cbb367b625cf28297f6e5955966fe915465f4df
chiropteracollection-oss <chiroptera.collection@gmail.com>
2026-07-31 00:54:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
 const button = document.getElementById("enterButton");
 
-button.addEventListener("click", async () => {
+button.onclick = () => {
 
-    if (!window.ethereum) {
-        alert("Please install MetaMask.");
-        return;
-    }
+    checkNFT();
 
-    try {
-
-        await window.ethereum.request({
-            method: "eth_requestAccounts"
-        });
-
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-        const signer = provider.getSigner();
-
-        const address = await signer.getAddress();
-
-        alert("Wallet Connected:\n\n" + address);
-
-    } catch (error) {
-
-        console.error(error);
-        alert("Connection cancelled.");
-
-    }
-
-});
+};
