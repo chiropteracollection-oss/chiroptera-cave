d44ad23e31f74fdd555f1e156542f62dfd3e2244
chiropteracollection-oss <chiroptera.collection@gmail.com>
2026-07-31 09:36:26 +0100

    Update nfts.js

--- functions/nfts.js
+++ functions/nfts.js
@@ -2,22 +2,19 @@ export async function onRequestGet(context) {
   const wallet = new URL(context.request.url).searchParams.get("wallet");
 
   if (!wallet) {
-    return new Response(
-      JSON.stringify({ error: "Wallet address is required" }),
-      {
-        status: 400,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
-        }
+    return new Response(JSON.stringify({ error: "Wallet address is required" }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
       }
-    );
+    });
   }
 
   const apiKey = context.env.OPENSEA_API_KEY;
   const collectionSlug = "chiroptera-collection";
 
-  const url = https://api.opensea.io/api/v2/chain/ethereum/account/${wallet}/nfts?collection=${collectionSlug};
+  const url = `https://api.opensea.io/api/v2/chain/ethereum/account/${wallet}/nfts?collection=${collectionSlug}`;
 
   const response = await fetch(url, {
     headers: {
